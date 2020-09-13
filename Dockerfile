7a2a3d40afb875f50993a1427442c362269124b6
sildvauresberg <slickunit4265752@gmail.com>
2020-09-13 17:06:12 +0300

    fix: Dockerfile source dir

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM openjdk:14.0.1-slim-buster
-COPY *.jar /var/app.jar
+COPY build/libs/*.jar /var/app.jar
 EXPOSE 8080
 ENTRYPOINT ["java", "-jar", "/var/app.jar"]
--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,16 +1,14 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.beans.factory.annotation.Value;
 
 import java.nio.file.FileStore;
 import java.nio.file.FileSystems;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
