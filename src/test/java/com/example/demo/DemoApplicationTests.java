692dbcac059839c095c4c8fcd3d6458e484a1067
sildvauresberg <slickunit4265752@gmail.com>
2020-09-20 00:45:46 +0300

    test: Exclude AWS ContextCredentialsAutoConfiguration

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,9 +1,13 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.aws.autoconfigure.context.ContextCredentialsAutoConfiguration;
+
 
 @SpringBootTest
+@EnableAutoConfiguration(exclude = ContextCredentialsAutoConfiguration.class)
 class DemoApplicationTests {
 
 	@Test
