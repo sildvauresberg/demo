610cda15d49a891fb237a2e6156748593ca60c55
sildvauresberg <slickunit4265752@gmail.com>
2020-09-20 00:54:13 +0300

    test: Exclude ContextStackAutoConfiguration instead of ContextCredentialsAutoConfiguration

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -4,10 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.aws.autoconfigure.context.ContextCredentialsAutoConfiguration;
+import org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration;
 
 
 @SpringBootTest
-@EnableAutoConfiguration(exclude = ContextCredentialsAutoConfiguration.class)
+@EnableAutoConfiguration(exclude = ContextStackAutoConfiguration.class)
 class DemoApplicationTests {
 
 	@Test
